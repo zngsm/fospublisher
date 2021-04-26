da5f47c6272aab0375e23bde5c5389eb77b72e39
zngsm <68373473+zngsm@users.noreply.github.com>
2021-04-26 18:54:20 +0900

    [feat/BE] : db table 수정 + pasts User 반영
    - User반영
    - 404, 403 예외처리

--- server/models/BookPasts.js
+++ server/models/BookPasts.js
@@ -1,15 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
-  const BookPasts = sequelize.define('BookPasts',
-    {
-      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-      page: { type: DataTypes.INTEGER },
-      title: { type: DataTypes.STRING }
-    }
-  );
-  
+  const BookPasts = sequelize.define("BookPasts", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    page: { type: DataTypes.INTEGER },
+    title: { type: DataTypes.STRING },
+    size: { type: DataTypes.INTEGER },
+    skin: { type: DataTypes.INTEGER },
+    font: { type: DataTypes.INTEGER },
+    skin_color: { type: DataTypes.STRING },
+    font_color: { type: DataTypes.STRING },
+  });
+
   BookPasts.associate = (models) => {
     BookPasts.belongsTo(models.Users);
   };
 
   return BookPasts;
-}
+};

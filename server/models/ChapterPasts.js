069a75c5a778b1ca6ea227424f8cbe08f8e3105e
zngsm <68373473+zngsm@users.noreply.github.com>
2021-04-27 09:54:21 +0900

    [feat/BE] : DB 수정
    - default value 지정
    - null 값 허용/불허 구분

--- server/models/ChapterPasts.js
+++ server/models/ChapterPasts.js
@@ -1,12 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
   const ChapterPasts = sequelize.define("ChapterPasts", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    title: { type: DataTypes.STRING },
-    content: { type: DataTypes.TEXT },
-    year: { type: DataTypes.INTEGER },
-    order: { type: DataTypes.INTEGER },
-    page: { type: DataTypes.INTEGER },
-    check: { type: DataTypes.BOOLEAN },
+    title: { type: DataTypes.STRING, allowNull: true },
+    content: { type: DataTypes.TEXT, allowNull: true },
+    year: { type: DataTypes.INTEGER, allowNull: false },
+    order: { type: DataTypes.INTEGER, allowNull: false },
+    page: { type: DataTypes.INTEGER, allowNull: false },
+    check: { type: DataTypes.BOOLEAN, allowNull: false },
   });
 
   ChapterPasts.associate = (models) => {
--- server/models/Users.js
+++ server/models/Users.js
@@ -1,25 +1,21 @@
-const moment = require('moment');
+const moment = require("moment");
 
 module.exports = (sequelize, DataTypes) => {
-  const Users = sequelize.define('Users',
-    {
-      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-      username: { type: DataTypes.STRING },
-      password: { type: DataTypes.STRING },
-      salt: { type: DataTypes.STRING },
-      birthday: { type: DataTypes.DATE },
-      nickname: { type: DataTypes.STRING },
-      introduce: { type: DataTypes.TEXT },
-      img: { type: DataTypes.STRING },
-      question: { type: DataTypes.INTEGER },
-      answer: { type: DataTypes.STRING }
-    }
-  );
+  const Users = sequelize.define("Users", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    username: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    salt: { type: DataTypes.STRING, allowNull: false },
+    birthday: { type: DataTypes.DATE, allowNull: false },
+    nickname: { type: DataTypes.STRING, allowNull: false },
+    introduce: { type: DataTypes.TEXT, allowNull: true },
+    img: { type: DataTypes.STRING, allowNull: true },
+    question: { type: DataTypes.INTEGER, allowNull: false },
+    answer: { type: DataTypes.STRING, allowNull: false },
+  });
 
-  Users.prototype.dateFormat = (date) => (
-    moment(date).format('YYYY-MM-DD')
-  );
-  
+  Users.prototype.dateFormat = (date) => moment(date).format("YYYY-MM-DD");
+
   Users.associate = (models) => {
     Users.hasMany(models.BookPasts);
   };
@@ -29,4 +25,4 @@ module.exports = (sequelize, DataTypes) => {
   };
 
   return Users;
-}
+};

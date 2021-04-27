a2e79b66f0619822259795b143376d32119c3554
윤지해 <stanajh@naver.com>
2021-04-27 11:55:12 +0900

    [feat/FE] : vuetify 추가

--- electron/src/renderer/main.js
+++ electron/src/renderer/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
+import vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 import App from './App'
 import router from './router'
@@ -12,6 +14,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   components: { App },
+  vuetify,
   router,
   store,
   template: '<App/>'

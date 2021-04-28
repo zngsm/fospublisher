2a19235c6aec00dbb6baf4ea02c080cd9ed613e1
zngsm <68373473+zngsm@users.noreply.github.com>
2021-04-28 23:08:52 +0900

    [feat/BE] : timeline 기능 추가
    - 타임라인 read
    - 타임라인 update
    - swagger 추가
    
    Resolves : #resolve S04P31B201-113, #resolve S04P31B201-114

--- server/controllers/timelines/timelines.ctrl.js
+++ server/controllers/timelines/timelines.ctrl.js
@@ -1,9 +1,49 @@
-// const models = require('../../models');
+const models = require("../../models");
 
-exports.get_timelines_read = (req, res) => {
+exports.get_timelines_read = async (req, res) => {
+  let context = {};
+  const book = await models.BookPasts.findOne({
+    where: { UserId: res.locals.userId },
+  });
+  models.ChapterPasts.findAll({
+    where: { BookPastId: book.id },
+    order: [["year"], ["order"]],
+  }).then((result) => {
+    result.forEach((chapter) => {
+      const year = chapter["dataValues"]["year"];
+      let node = mkChapterObj(chapter);
+      if (year in context === false) {
+        context[year] = [];
+      }
+      context[year].push(node);
+    });
+    res.send(context);
+  });
+};
 
-}
+const mkChapterObj = (chapter) => {
+  let node = {};
+  node["title"] = chapter["dataValues"]["title"];
+  node["id"] = chapter["dataValues"]["id"];
+  node["order"] = chapter["dataValues"]["order"];
+  node["check"] = chapter["dataValues"]["check"];
+  return node;
+};
 
-exports.put_timelines_edit = (req, res) => {
-
-}
+exports.put_timelines_edit = async (req, res) => {
+  let body = req.body;
+  const book = await models.BookPasts.findOne({
+    where: { UserId: res.locals.userId },
+  });
+  for (const key in body) {
+    body[key].forEach((chapter) => {
+      let order = {
+        order: chapter.order,
+      };
+      models.ChapterPasts.update(order, {
+        where: { id: chapter.id, BookPastId: book.id },
+      });
+    });
+  }
+  res.status(201).send({ success: "수정되었습니다." });
+};

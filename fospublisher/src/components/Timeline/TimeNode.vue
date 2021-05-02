ff78293e2646295679ea3eefb2cba721ea971db7
zngsm <68373473+zngsm@users.noreply.github.com>
2021-05-02 17:38:04 +0900

    [feat/FE] : 연표 초안 완성
    - 더미데이터상에서 연표 생성, 추가시 기차길 표현

---
+++ fospublisher/src/components/Timeline/TimeNode.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div v-for="(timeline, id) in timelines" :key="id">
+      <div v-if="idx == 0" class="timeline-title-top"></div>
+      <div v-if="timeline.status == 'start'">
+        <div class="timeline-title">
+          시작점1111
+        </div>
+        <div class="timeline-title-bottom"></div>
+      </div>
+      <div v-else-if="timeline.order == 0">
+        <div>
+          <div class="timeline-title">
+            {{ timeline.title }}
+          </div>
+          <div class="timeline-title-bottom"></div>
+        </div>
+        <div class="timeline-year">
+          {{timeline.year}}
+        </div>
+      </div>
+      <div v-else>
+        <div class="timeline-title">
+          {{ timeline.title }}
+        </div>
+        <div class="timeline-title-bottom"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Timenode',
+  props: {
+    timelines: Array,
+    idx: String,
+  },
+  data() {
+    return {
+      
+    };
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style scoped>
+
+</style>

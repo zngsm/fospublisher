0f83f1e0494cecbdcfc82a5d59b87fb306310678
zngsm <68373473+zngsm@users.noreply.github.com>
2021-05-07 22:30:59 +0900

    [feat/FE] : 책 CREATE 페이지
    - 에디터 반영해서 책 작성 페이지 생성
    - 신규 작성시만 반영
    - 자동저장/수정 미반영

---
+++ fospublisher/src/components/ConfirmModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      {{ headerTitle }}
+    </v-card-title>
+
+    <v-card-text>
+      {{ bodyContent }}
+    </v-card-text>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+
+      <v-btn small color="#231815" outlined @click="$emit('stayHere')">
+        취소
+      </v-btn>
+
+      <v-btn small color="#231815" outlined @click="$emit('goToNext')">
+        확인
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ConfirmModal",
+  props: {
+    headerTitle: {
+      type: String,
+      require: false,
+    },
+    bodyContent: {
+      type: String,
+      require: false,
+    },
+    moveTo: {
+      type: String,
+      require: false,
+    },
+  },
+};
+</script>
--- fospublisher/src/views/past/CreatePast.vue
+++ fospublisher/src/views/past/CreatePast.vue
@@ -1,39 +1,70 @@
 <template>
-  <div class="q-container">
-    <div class="left">
-      <h1>오늘의 질문</h1>
-      <h2>{{ question }}</h2>
-      <button @click="getTodayQuestion">질문 받기</button>
+  <div class="create-container">
+    <div class="create-left">
+      <h1 class="create-left-question-title text-center kukde-bold">
+        오늘의 질문
+      </h1>
+      <div class="create-left-question-content kekde-light">{{ question }}</div>
+      <button
+        class="create-left-question-button kekde-light"
+        @click="getTodayQuestion"
+      >
+        다른 질문 받기
+      </button>
     </div>
-    <div class="right">
-      <form @submit="onSubmit">
-        <input v-model="form.title" placeholder="제목" />
-        <h2>언제 있었던 일인가요?</h2>
-        <input v-model="form.year" placeholder="예: 1995" />년
-        <!-- 나중에 에디터 자리 -->
-        <input
-          v-model="form.content"
-          placeholder="그 때의 기억을 떠올려보세요"
-        />
+    <div class="create-right">
+      <form @submit="onSubmit" class="create-right-form">
+        <v-text-field v-model="form.title" label="제목" required></v-text-field>
+        <div class="d-flex create-right-year">
+          <div class="create-right-year-label">
+            언제 있었던 일인가요?
+          </div>
+          <div class="create-right-select-year">
+            <v-select
+              v-model="form.year"
+              :items="items"
+              label="년도"
+              dense
+            ></v-select>
+          </div>
+        </div>
+        <div class="create-editor">
+          <Editor :content="content" />
+        </div>
+        <div>
+          {{ content }}
+        </div>
       </form>
-      <button @click="onSubmit">발행</button>
+      <div class="create-right-btn">
+        <v-btn large>취소</v-btn>
+        <v-btn large @click="onSubmit">발행</v-btn>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { getQuestion, createPastChapter } from "@/api/past.js";
+import Editor from "@fospublisher/vue-text-editor";
 export default {
   name: "PastCreate",
+  components: {
+    Editor,
+  },
   props: {
     bookInfo: {
       type: Object,
     },
+    New: {
+      type: Boolean,
+    },
   },
   data() {
     return {
-      id: 1,
+      id: null,
+      questionId: null,
       question: "?",
+      content: null,
       form: {
         title: "",
         content: "",
@@ -41,13 +72,28 @@ export default {
         check: false,
       },
       interval: false,
+      items: [],
     };
   },
   methods: {
+    readContent() {
+      console.log(
+        window.richTextField.document.getElementsByTagName("body")[0].outerHTML
+      );
+    },
     getTodayQuestion() {
+      if (localStorage.getItem("question_num")) {
+        this.questionId = localStorage.getItem("question_num");
+      } else {
+        this.questionId = 1;
+      }
       getQuestion(
-        this.id,
-        (res) => ((this.id = res.data.id), (this.question = res.data.question)),
+        this.questionId,
+        (res) => {
+          this.questionId = res.data.id;
+          localStorage.setItem("question_num", this.questionId);
+          this.question = res.data.question;
+        },
         (err) => console.error(err)
       );
     },
@@ -57,6 +103,9 @@ export default {
       this.interval = false;
       clearInterval(this.interval);
       // 에디터 연결 시, 에디터의 내용 jquery로 잡아서 content로 보내주기
+      this.form.content = window.richTextField.document.getElementsByTagName(
+        "body"
+      )[0].outerHTML;
       createPastChapter(
         this.form,
         (res) => {
@@ -92,9 +141,24 @@ export default {
         }, 5000);
       }
     },
+    calculateYear() {
+      let today = new Date();
+      let year = today.getFullYear();
+      for (let a = 0; a <= 100; a++) {
+        this.items.push(year - a);
+      }
+    },
   },
   mounted() {
-    this.tempStore();
+    // document.getElementsByClassName("editorWidth")[0].style.width = "80px";
+    this.getTodayQuestion();
+    this.calculateYear();
+    // this.tempStore();
+  },
+  beforeRouteLeave(to, from, next) {
+    if (window.confirm("정말로 떠나시겠습니까?")) {
+      next();
+    }
   },
 };
 </script>

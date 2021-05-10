1b3f79e6c6c3190f1a768a0cbe2aec0294a3c205
jowoori <dnfl531@naver.com>
2021-05-10 10:28:51 +0900

    [feat]: 스타일링 옵션 적용

---
+++ open-source/fospublisher-vue-text-editor/dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import FospublisherVueTextEditor from '@/fospublisher-vue-text-editor.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    FospublisherVueTextEditor
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <fospublisher-vue-text-editor />
+  </div>
+</template>
--- open-source/src/index.js
+++
@@ -1,5 +0,0 @@
-import Editor from './components/Editor.vue'
-
-export default {
-  Editor,
-}

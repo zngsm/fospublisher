61a18cf83e58540039dfc2af9c6b28139dc26758
zngsm <68373473+zngsm@users.noreply.github.com>
2021-05-13 15:44:08 +0900

    [feat/BE] : 유저 검색기능
    - 유저 검색
    - 해당 검색어와 같은 유저 우선
    - 그 다음부터는 해당 검색어가 포함된 유저

---
+++ server/controllers/search/search.ctrl.js
@@ -0,0 +1,45 @@
+const { Sequelize } = require("sequelize");
+const models = require("../../models");
+
+exports.search_user = async (req, res) => {
+  const user = req.params.user;
+  let context = [];
+
+  await models.Users.findOne({
+    where: {
+      [Sequelize.Op.or]: [{ username: { [Sequelize.Op.like]: user } }],
+    },
+  }).then((result) => {
+    let userInfo = {
+      id: result.id,
+      username: result.username,
+      nickname: result.nickname,
+      img: result.img,
+      birthday: result.birthday,
+      introduce: result.introduce,
+    };
+    context.push(userInfo);
+  });
+
+  await models.Users.findAll({
+    where: {
+      [Sequelize.Op.and]: [
+        { username: { [Sequelize.Op.like]: `%${user}%` } },
+        { username: { [Sequelize.Op.notLike]: `${user}` } },
+      ],
+    },
+  }).then((results) => {
+    for (let result of results) {
+      let userInfo = {
+        id: result.id,
+        username: result.username,
+        nickname: result.nickname,
+        img: result.img,
+        birthday: result.birthday,
+        introduce: result.introduce,
+      };
+      context.push(userInfo);
+    }
+  });
+  res.send(context);
+};

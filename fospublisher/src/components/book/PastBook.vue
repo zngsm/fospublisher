fa0d59184f71fb3a55cad0e93bc3e1958220a57d
kim mun jeong <shzmfkdl@naver.com>
2021-05-14 18:18:08 +0900

    [fix/FE] : 쓰기모드 진입 시 상태 추가

--- fospublisher/src/components/book/PastBook.vue
+++ fospublisher/src/components/book/PastBook.vue
@@ -49,7 +49,7 @@ export default {
     sendBookInfo() {
       this.$router.push({
         name: "ReadPast",
-        params: { bookInfo: this.bookInfo },
+        params: { bookInfo: this.bookInfo, status: "PAST" },
       });
     },
   },

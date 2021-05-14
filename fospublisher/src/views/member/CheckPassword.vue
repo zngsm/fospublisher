a19cc4fbb8ab4aaf6a2e5b91bdfbb378c62650eb
윤지해 <stanajh@naver.com>
2021-05-15 04:38:25 +0900

    [style/FE] :비밀번호변경 관련 전체 css 및 폰트 수정

--- fospublisher/src/views/member/CheckPassword.vue
+++ fospublisher/src/views/member/CheckPassword.vue
@@ -1,60 +1,62 @@
 <template>
-  <v-container fluid class="member-kwandong">
-    <v-row class="align-center" style="height: 100vh">
-      <v-col cols="6"> </v-col>
+  <v-container fluid grid-list-md fill-height>
+    <v-row>
+      <v-col cols="6" class="d-flex align-center justify-end pa-16">
+        <div>
+          <v-img
+            contain
+            src="../../assets/logo.png"
+            alt="역전출판사"
+            height="300px"
+          />
+        </div>
+      </v-col>
       <v-divider vertical></v-divider>
-      <v-col cols="6">
-        <v-row justify="center">
-          <div style="width: 30vw">
-            <p class="member-kukde-light">비밀번호 확인</p>
-            <div class="d-flex align-center flex-column">
-              <div style="width: 25vw">
-                <v-form ref="form" v-on:submit.prevent="changePassword">
-                  <v-text-field
-                    label="현재 비밀번호"
-                    v-model.lazy="password"
-                    type="password"
-                    autocomplete="off"
-                    @keyup.enter="changePassword"
-                    :rules="[(v) => !!v || '현재 비밀번호는 필수입니다.']"
-                  >
-                  </v-text-field>
-                </v-form>
-              </div>
-              <div class="text-center">
-                <v-row class="justify-center">
-                  <v-btn
-                    color="#fff"
-                    class="ma-5"
-                    style="width: 10vw"
-                    to="/"
-                    x-large
-                    >취소</v-btn
-                  >
-                  <v-btn
-                    color="#231815"
-                    class="ma-5"
-                    style="width: 10vw"
-                    dark
-                    x-large
-                    type="submit"
-                    @click="changePassword"
-                    >비밀번호 변경</v-btn
-                  >
-                  <!-- Start 비밀번호 확인 모달 -->
-                  <v-dialog v-model="dialog" width="25vw">
-                    <message-modal
-                      v-if="noData"
-                      body-content="비밀번호를 다시 확인해주세요."
-                      @submit="closeDialog"
-                    />
-                  </v-dialog>
-                  <!-- End  비밀번호 확인 모달 -->
-                </v-row>
-              </div>
+      <v-col cols="6" class="d-flex align-center pa-16">
+        <div>
+          <p class="page-title-bold">비밀번호 확인</p>
+          <br />
+          <div class="d-flex align-center flex-column">
+            <div class="member-input-size">
+              <v-form ref="form" v-on:submit.prevent="changePassword">
+                <v-text-field
+                  label="현재 비밀번호"
+                  v-model.lazy="password"
+                  type="password"
+                  autocomplete="off"
+                  @keyup.enter="changePassword"
+                  :rules="[(v) => !!v || '현재 비밀번호는 필수입니다.']"
+                >
+                </v-text-field>
+              </v-form>
+            </div>
+            <div class="text-center">
+              <v-row class="justify-center">
+                <v-btn color="#fff" class="ma-5 member-btn-size" large to="/"
+                  >취소</v-btn
+                >
+                <v-btn
+                  color="#231815"
+                  class="ma-5 member-btn-size"
+                  dark
+                  large
+                  type="submit"
+                  @click="changePassword"
+                  >비밀번호 변경</v-btn
+                >
+                <!-- Start 비밀번호 확인 모달 -->
+                <v-dialog v-model="dialog" width="300px">
+                  <message-modal
+                    v-if="noData"
+                    body-content="비밀번호를 다시 확인해주세요."
+                    @submit="closeDialog"
+                  />
+                </v-dialog>
+                <!-- End  비밀번호 확인 모달 -->
+              </v-row>
             </div>
           </div>
-        </v-row>
+        </div>
       </v-col>
     </v-row>
   </v-container>

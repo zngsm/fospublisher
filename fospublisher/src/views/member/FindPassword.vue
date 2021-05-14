3e246d3fd0d9c0776a88f3d564cf11bc532cc7ce
윤지해 <stanajh@naver.com>
2021-05-15 04:13:34 +0900

    [style/FE] : 코드 스타일 전체 수정 및 font 변경

--- fospublisher/src/views/member/FindPassword.vue
+++ fospublisher/src/views/member/FindPassword.vue
@@ -1,71 +1,76 @@
 <template>
-  <v-container fluid class="member-kwandong">
-    <v-row class="align-center" style="height: 100vh">
-      <v-col cols="6"> </v-col>
+  <v-container fluid grid-list-md fill-height>
+    <v-row>
+      <v-col cols="6" class="d-flex align-center justify-end pa-16">
+        <div>
+          <v-img
+            contain
+            src="../../assets/logo.png"
+            alt="역전출판사"
+            height="300px"
+          />
+        </div>
+      </v-col>
       <v-divider vertical></v-divider>
-      <v-col cols="6">
-        <v-row justify="center">
-          <div style="width: 30vw">
-            <p class="member-kukde-light">비밀번호 찾기</p>
-            <div class="d-flex align-center flex-column">
-              <div style="width: 25vw">
-                <v-text-field
-                  label="* 아이디"
-                  v-model="username"
-                ></v-text-field>
-                <password-question isRequired @input="questionReceive" />
-                <v-text-field
-                  label="* 비밀번호 찾기 답변"
-                  v-model="answer"
-                  @keyup.enter="findPassword"
-                ></v-text-field>
-                <div class="text-center">
-                  <v-btn
-                    block
-                    color="#231815"
-                    class="my-6"
-                    dark
-                    depressed
-                    x-large
-                    type="submit"
-                    @click="findPassword"
-                    >비밀번호 찾기</v-btn
-                  >
-                  <!-- Start 비밀번호찾기 모달 -->
-                  <v-dialog v-model="dialog" width="25vw">
-                    <message-modal
-                      v-if="isFailedFind"
-                      body-content="회원정보를 찾을 수 없습니다.<br />답변이 기억나지 않는다면,<br />201publisher@gmail.com으로 문의바랍니다."
-                      @submit="closeDialog"
-                    />
-                    <message-modal
-                      v-if="isEmpty"
-                      body-content="입력이 올바르지 않습니다."
-                      @submit="closeDialog"
-                    />
-                  </v-dialog>
-                  <!-- End 비밀번호찾기 모달 -->
-                </div>
+      <v-col cols="6" class="d-flex align-center pa-16">
+        <div>
+          <p class="page-title-bold">비밀번호 찾기</p>
+          <br />
+          <div class="d-flex align-center flex-column pl-8">
+            <div class="member-input-size">
+              <v-text-field label="* 아이디" v-model="username"></v-text-field>
+              <password-question isRequired @input="questionReceive" />
+              <v-text-field
+                label="* 비밀번호 찾기 답변"
+                v-model="answer"
+                @keyup.enter="findPassword"
+              ></v-text-field>
+              <div class="text-center">
+                <v-btn
+                  block
+                  color="#231815"
+                  class="my-6"
+                  dark
+                  depressed
+                  x-large
+                  type="submit"
+                  @click="findPassword"
+                  >비밀번호 찾기</v-btn
+                >
+                <!-- Start 비밀번호찾기 모달 -->
+                <v-dialog v-model="dialog" width="300px">
+                  <message-modal
+                    v-if="isFailedFind"
+                    body-content="회원정보를 찾을 수 없습니다.<br />답변이 기억나지 않는다면,<br />201publisher@gmail.com으로 문의바랍니다."
+                    @submit="closeDialog"
+                  />
+                  <message-modal
+                    v-if="isEmpty"
+                    body-content="입력이 올바르지 않습니다."
+                    @submit="closeDialog"
+                  />
+                </v-dialog>
+                <!-- End 비밀번호찾기 모달 -->
               </div>
-              <v-row class="justify-center">
-                <v-btn
-                  text
-                  :ripple="false"
-                  class="text-decoration-underline"
-                  to="/register"
-                  >회원가입</v-btn
-                >
-                <v-btn
-                  text
-                  :ripple="false"
-                  class="text-decoration-underline"
-                  to="/login"
-                  >로그인</v-btn
-                >
-              </v-row>
             </div>
+            <v-row class="justify-center">
+              <v-btn
+                text
+                :ripple="false"
+                class="text-decoration-underline"
+                to="/register"
+                >회원가입</v-btn
+              >
+              <v-btn
+                text
+                :ripple="false"
+                class="text-decoration-underline"
+                to="/login"
+                >로그인</v-btn
+              >
+            </v-row>
           </div>
-        </v-row>
+        </div>
       </v-col>
     </v-row>
   </v-container>

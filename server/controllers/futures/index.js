9c25e45dd0092a7c211a92d4a9bc548336df11a6
zngsm <68373473+zngsm@users.noreply.github.com>
2021-05-15 17:24:08 +0900

    [feat/BE] : 과거/미래 책 표지 정보 api
    - pasts(or futures)/cover GET 요청

--- server/controllers/futures/index.js
+++ server/controllers/futures/index.js
@@ -5,6 +5,8 @@ const ctrl = require("./futures.ctrl");
 
 router.post("/", verifyToken, ctrl.post_futures_write);
 
+router.get("/cover", verifyToken, ctrl.get_futures_cover);
+
 router.put("/:chapter_id", verifyToken, ctrl.put_futures_edit);
 
 router.delete("/:chapter_id", verifyToken, ctrl.delete_futures_delete);
